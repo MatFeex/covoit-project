e629cfe7d5af00edba606fabbf209b2abd935a55
mfresson <mathieu.fresson@epfedu.fr>
2023-03-29 16:12:08 +0200

    register, login, logout

--- config/models.py
+++ config/models.py
@@ -1,7 +1,18 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 # User Model Customization
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_joined = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
 
 class Course(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- config/serializers.py
+++ config/serializers.py
@@ -1,20 +1,44 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from .models import User
+from django.contrib.auth.hashers import make_password
+
+# SERIALIZERS : Serializers allow complex data such as querysets and model instances to be converted to native Python datatypes
 
 
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id','username','email')
+        fields = ('id','first_name', 'last_name', 'email','date_joined')
+
 
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = User
-        fields = ('id','username', 'email', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id','first_name','last_name', 'email', 'password')
+        extra_kwargs = extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            },
+            'email': {
+                'validators': [
+                    UniqueValidator(queryset=User.objects.all())
+                ]
+            }
+        }
 
     def create(self, validated_data):
-        user = User.objects.create_user(validated_data['email'], validated_data['password'])
+        validated_data['password'] = make_password(validated_data['password']) # hash the password before saving user in database
+        user = User.objects.create(**validated_data)
         return user
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.save()
+        return instance
