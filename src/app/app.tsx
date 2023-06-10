839241d50d5b2db7f1cff50e71d0dc5809813c12
Mathieu FRESSON <mathieufresson@gmail.com>
2023-06-10 17:27:38 +0200

    dialog & erros handling : useInfo

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -9,36 +9,38 @@ import ListeTrajet from "./pages/liste_trajet/liste_trajet";
 import Logout from "./pages/logout/logout";
 import Erreur from "./pages/erreur/erreur";
 
-import "../../node_modules/bootstrap/js/dist/collapse";
-import "../../node_modules/bootstrap/js/dist/dropdown";
-import "../../node_modules/bootstrap/js/dist/modal";
+import "bootstrap/js/dist/collapse";
+import "bootstrap/js/dist/dropdown";
+import "bootstrap/js/dist/modal";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Detail_Trajet from "./pages/detail_trajet/detail_trajet";
 import Profil from "./pages/profil/profil";
-
+import { InfoProvider } from "./context/InfoContext";
 import "./style/style.scss";
 import ModifProfil from "./pages/modif_profil/modif_profil";
 
 export default function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Accueil />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/add" element={<AjoutTrajet />} />
-          <Route path="/list" element={<ListeTrajet />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="/course/:id" element={<Detail_Trajet />} />
-          <Route path="/profil/:id" element={<Profil />} />
-          <Route path="/profil" element={<ModifProfil />} />
-          <Route path="*" element={<Erreur />} />
-        </Routes>
-        {/* <Footer /> */}
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route element={<InfoProvider />}>
+						<Route path="/" element={<Accueil />} />
+						<Route path="/login" element={<Login />} />
+						<Route path="/signin" element={<Signin />} />
+						<Route path="/add" element={<AjoutTrajet />} />
+						<Route path="/list" element={<ListeTrajet />} />
+						<Route path="/logout" element={<Logout />} />
+						<Route path="/course/:id" element={<Detail_Trajet />} />
+						<Route path="/profil/:id" element={<Profil />} />
+						<Route path="/profil" element={<ModifProfil />} />
+						<Route path="*" element={<Erreur />} />
+					</Route>
+				</Routes>
+				{/* <Footer /> */}
+			</Router>
+		</div>
+	);
 }
---
+++ src/app/context/InfoContext.jsx
@@ -0,0 +1,72 @@
+import React, { useState, createContext } from "react";
+import { Outlet } from "react-router-dom";
+import { useMemo } from "react";
+import MuiAlert from "@mui/material/Alert";
+import Snackbar from "@mui/material/Snackbar";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+
+
+const InfoContext = createContext();
+
+export default InfoContext;
+
+export const InfoProvider = () => {
+
+	const [openError, setOpenError] = useState(false);
+	const [textError, setTextError] = useState("Error !");
+	const [openSuccess, setOpenSuccess] = useState(false);
+	const [textSuccess, setTextSuccess] = useState("Success !");
+	const [openBackdrop, setOpenBackdrop] = useState(false);
+
+    const Alert = React.forwardRef(function Alert(props, ref) {
+        return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+    });
+
+	const handleClose = (e, reason) => {
+		if (reason === "clickaway") {
+			return;
+		}
+		setOpenSuccess(false);
+		setOpenError(false);
+	};
+
+	const contextData = useMemo(() => {
+		return {
+			openError,
+			setOpenError,
+			textError,
+			setTextError,
+			openSuccess,
+			setOpenSuccess,
+			textSuccess,
+			setTextSuccess,
+			openBackdrop,
+			setOpenBackdrop,
+		};
+	}, [openError, setOpenError, textError, setTextError, openSuccess, setOpenSuccess, textSuccess, setTextSuccess, openBackdrop, setOpenBackdrop]);
+
+
+	return (
+		<InfoContext.Provider value={contextData}>
+			<Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={openBackdrop} onClick={handleClose}>
+				<CircularProgress color="inherit" />
+			</Backdrop>
+
+			<Snackbar open={openError} autoHideDuration={5000} onClose={handleClose}>
+				<Alert onClose={handleClose} severity="error" sx={{ width: "100%" }}>
+					{textError}
+				</Alert>
+			</Snackbar>
+
+			<Snackbar open={openSuccess} autoHideDuration={5000} onClose={handleClose}>
+				<Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+					{textSuccess}
+				</Alert>
+			</Snackbar>
+
+			<Outlet />
+
+		</InfoContext.Provider>
+	);
+};
